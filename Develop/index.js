9ddcf1bef18c4f6fbbc4b653a0ec3117e8728e06
Christopher Garcia <chris@christophersmbp.lan>
2021-11-03 20:12:07 -0400

    prompts the user with the questions about their project

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,41 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
 const questions = [];
+inquirer
+.prompt([
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Description of your project: ',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'Installation instructions: ',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'Usage information about your project: ',
+        name: 'usageInfo',
+    },
+    {
+        type: 'input',
+        message: 'Contribution guidelines: ',
+        name: 'guidelines',
+    },
+    {
+        type: 'input',
+        message: 'Test instructions: ',
+        name: 'test'
+    }
+  ])
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
